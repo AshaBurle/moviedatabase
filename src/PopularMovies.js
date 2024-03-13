b605177720bcededa6ca4ec580d6c21415d81d4c
asha <ashaburle06@gmail.com>
2024-03-13 06:10:54 +0000

    solution

--- src/PopularMovies.js
+++ src/PopularMovies.js
@@ -4,12 +4,12 @@ const PopularMovies = () => {
   const [movieData, setMovieData] = useState([])
   const [searchInput, setSearchInput] = useState([])
   const [activePage, setActivePage] = useState('Popular')
+  const [currentPage, setCurrentPage] = useState(1)
 
   useEffect(() => {
-    const getPopularMoviesURL =
-      'https://api.themoviedb.org/3/movie/popular?api_key=b0c5333050f9fa66124a9eeef3d0b547&language=en-US&page=1'
+    const getMoviesURL = `https://api.themoviedb.org/3/movie/${activePage.toLowerCase()}?api_key=b0c5333050f9fa66124a9eeef3d0b547&language=en-US&page=${currentPage}`
 
-    fetch(getPopularMoviesURL)
+    fetch(getMoviesURL)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok')
@@ -20,18 +20,28 @@ const PopularMovies = () => {
         setMovieData(data.results)
       })
       .catch(error => console.error('Error fetching data:', error))
-  }, [])
+  }, [activePage, currentPage])
+
+  const handleNextPage = () => {
+    setCurrentPage(prevPage => prevPage + 1)
+  }
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(prevPage => prevPage - 1)
+    }
+  }
 
   return (
     <div>
-      <h2>movieDB</h2>
+      <h1>movieDB</h1>
       <h2>Popular</h2>
       <h2>Top Rated</h2>
       <h2>Upcoming</h2>
       <nav>
         <input
-          type="text"
-          placeholder="Search"
+          type='text'
+          placeholder='Search'
           value={searchInput}
           onChange={e => setSearchInput(e.target.value)}
         />
@@ -46,20 +56,31 @@ const PopularMovies = () => {
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
             />
-
-            {/* Conditional rendering of "View Details" button based on active page */}
             {activePage === 'Popular' ||
-            activePage === 'Top Rated' ||
-            activePage === 'Upcoming' ? (
-              <button
-                onClick={() => console.log(`View details for ${movie.title}`)}
-              >
-                View Details
-              </button>
-            ) : null}
+              activePage === 'Top Rated' ||
+              (activePage === 'Upcoming' && (
+                <button
+                  onClick={() => console.log(`View details for ${movie.title}`)}
+                >
+                  View Details
+                </button>
+              ))}
           </div>
         ))}
       </section>
+
+      {/* Page navigation for all pages */}
+      {activePage === 'Popular' ||
+      activePage === 'Top Rated' ||
+      activePage === 'Upcoming' ? (
+        <div>
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            Prev
+          </button>
+          <p>{currentPage}</p>
+          <button onClick={handleNextPage}>Next</button>
+        </div>
+      ) : null}
     </div>
   )
 }
